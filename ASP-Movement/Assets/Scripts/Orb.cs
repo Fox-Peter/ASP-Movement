d4cb82d7b02c9ca887d9df4d49d16bf3588aad59
daylinawo <frank.mathurin@gmail.com>
2021-03-20 20:56:24 +0000

    Raycasting to toggle Orb glow
    Aim at orb object and use left mouse button to toggle the glowing effect

--- ASP-Movement/Assets/Scripts/Orb.cs
+++ ASP-Movement/Assets/Scripts/Orb.cs
@@ -4,28 +4,5 @@ using UnityEngine;
 
 public class Orb : MonoBehaviour
 {
-    //script currently unnecessary
 
-    [SerializeField] private Material m_on;
-    [SerializeField] private Material m_off;
-    [SerializeField] private Attractor m_attractor;
-    [SerializeField] private MeshRenderer m_renderer;
-
-    private bool m_state;
-    
-    public void Toggle()
-    {
-        m_state = !m_state;
-
-        if (m_state)
-        {
-            m_renderer.material = m_on;
-            m_attractor.enabled = true;
-        }
-        else
-        {
-            m_renderer.material = m_off;
-            m_attractor.enabled = false;
-        }
-    }
 }
---
+++ ASP-Movement/Assets/Scripts/OrbGlow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbGlow : MonoBehaviour
+{
+    [SerializeField] private float pulseTime;
+
+    [ColorUsageAttribute(true, true)]
+    [SerializeField] private Color glowColor;
+
+    [SerializeField] private MeshRenderer m_renderer;
+
+    [SerializeField] private bool m_state;
+
+    private void Start()
+    {
+        m_state = true;
+        Toggle();
+    }
+    public void Toggle()
+    {
+        m_state = !m_state;
+
+        if (m_state)
+        {
+            m_renderer.material.SetColor("_GlowColor", glowColor);
+            m_renderer.material.SetFloat("_PulseTime", pulseTime);
+        }
+        else
+        {
+            m_renderer.material.SetColor("_GlowColor", Color.black);
+            m_renderer.material.SetFloat("_PulseTime", 0.0f);
+        }
+    }
+}
